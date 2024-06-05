faceb2c365e705104730a788bca977c08031f5c2
laolang2016 <xiaodaima2016@163.com>
2024-06-06 05:54:28 +0800

    ✨ feat(cpp_study/cmake-cpp-setup): 添加 lua

--- cpp_study/cmake-cpp-setup/src/app/main.cpp
+++ cpp_study/cmake-cpp-setup/src/app/main.cpp
@@ -1,3 +1,9 @@
+extern "C" {
+#include <lua/lauxlib.h>
+#include <lua/lua.h>
+#include <lua/lualib.h>
+}
+
 #include <iostream>
 #include <memory>
 
@@ -9,5 +15,21 @@ int main(int argc, char** argv) {
     UNUSED(argv);
     auto person = std::make_unique<Person>(18, "laolang");
     std::cout << "Hello . My name is " << person->name() << " ." << std::endl;
+
+    std::cout << "lua" << std::endl;
+
+    // 创建 lua 状态
+    lua_State* L = luaL_newstate();
+
+    // 启用基本的 lua 库
+    luaopen_base(L);
+
+    // 加载 lua 文件
+    luaL_loadfile(L, "main.lua");
+
+    // 运行 lua 文件
+    int ret = lua_pcall(L, 0, 0, 0);
+    std::cout << "ret:" << ret << std::endl;
+
     return 0;
 }
